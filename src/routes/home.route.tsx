03f2c4d5ac99d44db7f5cb56e89c834694708206
Chris Carrington <carrington.christopher@gmail.com>
2026-07-31 09:59:03 -0700

    bare

--- src/routes/home.route.tsx
+++ src/routes/home.route.tsx
@@ -7,27 +7,27 @@ import { counterSolid, counterVanilla } from '@hono-directives'
 const app = new Hono()
 
 app.get('/', async (c) => {
-  const faqItems = [
-    { question: 'What is this?', answer: 'This is a FAQ.' },
-    { question: 'How does it work?', answer: 'With directives and Solid.' },
-  ]
+//   const faqItems = [
+//     { question: 'What is this?', answer: 'This is a FAQ.' },
+//     { question: 'How does it work?', answer: 'With directives and Solid.' },
+//   ]
 
-  const html = await marked.parse(`# Marked in Hono
+//   const html = await marked.parse(`# Marked in Hono
 
-| Month    | Savings |
-| -------- | ------- |
-| January  | $250    |
-| February | $80     |
-| March    | $420    |
-`)
+// | Month    | Savings |
+// | -------- | ------- |
+// | January  | $250    |
+// | February | $80     |
+// | March    | $420    |
+// `)
 
   return c.render(
     <>
       <h1>Home!</h1>
-      <div data-directive={counterSolid()}></div>
+      {/* <div data-directive={counterSolid()}></div>
       <button data-directive={counterVanilla()} type="button"></button>
       <FAQ items={faqItems} />
-      <div dangerouslySetInnerHTML={{__html: html}} />
+      <div dangerouslySetInnerHTML={{__html: html}} /> */}
     </>
   )
 })
