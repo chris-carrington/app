872fa8925546ea040f5e46486b378ccbf8340726
Chris Carrington <carrington.christopher@gmail.com>
2026-08-21 00:07:00 -0700

    restore signup (the bug was env variable related)

--- src/auth/signUp.api.ts
+++ src/auth/signUp.api.ts
@@ -19,52 +19,39 @@ export default new Hono()
       const data = c.req.valid('json')
 
       try {
-        console.log('TURSO_DATABASE_URL:', env.TURSO_DATABASE_URL);
-        console.log('TURSO_AUTH_TOKEN:', env.TURSO_AUTH_TOKEN.slice(-3));
-
-        const db = drizzle(createClient({
-          url: env.TURSO_DATABASE_URL,
-          authToken: env.TURSO_AUTH_TOKEN,
-        }))
-
-        const test = await db
+        const contact = await db // get contact
           .select()
-          .from(Trade)
-          console.log(test)
-          return c.json(test)
-        // const contact = await db // get contact
-        //   .select()
-        //   .from(Contact)
-        //   .where(eq(Contact.email, data.email))
-        //   .limit(1)
-        //   .get()
+          .from(Contact)
+          .where(eq(Contact.email, data.email))
+          .limit(1)
+          .get()
 
-        // if (!contact) { // IF new THEN add
-        //   await db.transaction(async (tx) => {
-        //     const person = await tx.insert(Person) // add Person
-        //       .values({
-        //         firstName: data.firstName,
-        //         lastName: data.lastName,
-        //       })
-        //       .returning({ id: Person.id })
-        //       .get()
+        if (!contact) { // IF new THEN add
+          await db.transaction(async (tx) => {
+            const person = await tx.insert(Person) // add Person
+              .values({
+                firstName: data.firstName,
+                lastName: data.lastName,
+              })
+              .returning({ id: Person.id })
+              .get()
 
-        //     await tx // add Contact
-        //       .insert(Contact)
-        //       .values({
-        //         personId: person.id,
-        //         email: data.email,
-        //         emailVerified: false,
-        //         sendNewsletter: true,
-        //       })
-        //   })
-        // }
+            await tx // add Contact
+              .insert(Contact)
+              .values({
+                personId: person.id,
+                email: data.email,
+                emailVerified: false,
+                sendNewsletter: true,
+              })
+          })
+        }
 
-        // const res = await signIn(data.email)
+        const res = await signIn(data.email)
 
-        // return res.status === 200
-        //   ? c.json({ success: true })
-        //   : c.json({ success: false, error: res.message }, res.status)
+        return res.status === 200
+          ? c.json({ success: true })
+          : c.json({ success: false, error: res.message }, res.status)
       } catch (e) {
         console.error(e)
         return c.json({ success: false, error: 'An unexpected error happened' }, 500);
