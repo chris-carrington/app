3c26052945bddb7fe1cada35b0ef84168c22409a
Chris Carrington <carrington.christopher@gmail.com>
2026-09-16 13:12:39 -0700

    Update ObjectiveKanban.ts

--- src/objectives/ObjectiveKanban.ts
+++ src/objectives/ObjectiveKanban.ts
@@ -9,7 +9,7 @@ import { ObjectiveController } from '@src/objectives/ObjectiveController'
 import type { QueryObjectives, QueryObjective } from '@src/db/queryObjective'
 import { ObjectiveInUpShowModal } from '@src/objectives/ObjectiveInUpShowModal'
 import { formObjectiveValidator } from '@src/validators/inupObjective.validator'
-import { classNameAssignees, classNameColumn, classNameComment, classNameCount, classNameIsBeingDragged, classNameObjective, classNameObjectives, classNameSvg, classNameTags, classNameTitle, datasetColumnId, datasetOrder, fieldObjectiveInUpAssigneeIds, fieldObjectiveInUpTagIds, idObjectiveInUpForm, idObjectiveInUpModalComments, idObjectiveTemplate } from '@src/lib/dom'
+import { classNameAssignees, classNameColumn, classNameCount, classNameIsBeingDragged, classNameObjective, classNameObjectives, classNameSvg, classNameTags, classNameTitle, datasetColumnId, datasetOrder, fieldObjectiveInUpAssigneeIds, fieldObjectiveInUpTagIds, idObjectiveInUpForm, idObjectiveTemplate } from '@src/lib/dom'
 
 
 
